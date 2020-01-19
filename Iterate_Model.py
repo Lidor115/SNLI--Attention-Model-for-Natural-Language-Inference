1d59ff37eaffe10b36f50cd8c4afadfef1dee019
Tal Levy <tall@wix.com>
2020-01-19 15:24:55 +0200

    main changes

--- Iterate_Model.py
+++ Iterate_Model.py
@@ -1,6 +1,9 @@
 
 import torch
 import torch.nn as nn
+import time
+from Model import Encode, SelfAttention
+from Parser import Parser
 
 
 def calc_batch_accuracy(predictions, labels, idx_to_label):
@@ -25,6 +28,13 @@ def predict(model, windows, file_type, L2I, msg):
             file.write("{0}\n".format(y))
     file.close()
     return predictions
+
+
+def time_for_epoch(start, end):
+    end_to_end = end - start
+    minutes = int(end_to_end / 60)
+    seconds = int(end_to_end - (minutes * 60))
+    return minutes, seconds
 
 
 def train(model, train_set, optimizer, loss_fn, idx_to_label):
@@ -56,13 +66,33 @@ def evaluate(model, dev_set, loss_fn, idx_to_label):
     return float(epoch_loss) / sum_examples, float(epoch_acc) / sum_examples
 
 
-def trainer_loop(model, train_set, dev_set, idx_to_label, lr=0.01, epochs=10, msg=''):
+def iterate_model(model, train_set, dev_set, idx_to_label, lr=0.01, epochs=10, msg=''):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss = nn.CrossEntropyLoss()
     for epoch in range(epochs):
+        start_time = time.time()
         train_loss, train_acc, model = train(model, train_set, optimizer, loss, idx_to_label)
         dev_loss, dev_acc = evaluate(model, dev_set, loss, idx_to_label)
-        print('Epoch: ' + str(epoch + 1))
-        print(f'\tTrain Loss: {train_loss:.3f}, Train Acc: {train_acc * 100:2f}%')
-        print(f'\tDev Loss: {dev_loss:.3f}, Dev Acc: {dev_acc * 100:2f}%')
+        end_time = time.time()
+        epoch_mins, epoch_secs = time_for_epoch(start_time, end_time)
+        print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+        print(f'\tDev Loss: {dev_loss:.3f} | Dev Acc: {dev_acc * 100:.2f}%')
     return model
+
+
+def main():
+    train_parser = Parser('./Data/snli_1.0_train.jsonl')
+    data_train, max_len_train = train_parser.Parse()
+    dev_parser = Parser('./Data/snli_1.0_dev.jsonl')
+    data_dev, max_len_dev = dev_parser.Parse()
+    test_parser = Parser('./Data/snli_1.0_test.jsonl')
+    test_train, max_len_test = test_parser.Parse()
+    embedding_dim = 300
+    hidden_dim_1 = 1000
+    batch_size = 20
+    output_dim = 3
+
+
+if __name__ == "__main__":
+    main()
